fc27d7f0d7cffc2047368eb7e8d0675965a08a2d
vippujain <vippujain@gmail.com>
2017-04-20 22:49:01 +0530

    first

---
+++ src/main/java/com/infogain/App.java
@@ -0,0 +1,13 @@
+package com.infogain;
+
+/**
+ * Welcome to infogain!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to infogain!" );
+    }
+}
